f34e02aeb6ef1545d243a1d119ca1932479f061e
MatevzMiscic <matevz.miscic@gmail.com>
2020-12-01 14:14:47 +0100

    Napisal rešitev prve naloge

--- day1.ml
+++ day1.ml
@@ -1,16 +1,20 @@
+(*Treba je le pravilno nastimati a*)
+let a = [|100; 2; 3; 4; 6; 9; 10; 23; 30|]
 
-let a = [|2; 3; 4; 6; 9; 10; 23; 30|]
-
-let elementa_z_vsoto sum array = 
+let elementa_z_vsoto array sum= 
     let rec aux i j = 
         if i >= j then None
         else let s = array.(i) + array.(j) in 
             if s > sum then aux i (j - 1)
             else if s < sum then aux (i + 1) j
-            else Some (i, j)
+            else Some (array.(i), array.(j))
     in aux 0 (Array.length array - 1)
 
-let r = elementa_z_vsoto a 10;;
+let resi array = 
+    Array.sort (-) array;
+    let pair = elementa_z_vsoto array 10 in
+    match pair with
+    | None -> failwith "Napaka"
+    | Some (a, b) -> a * b
 
-let resi array = ()
-    
+let resitev = resi a
