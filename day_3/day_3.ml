81062741480ff310de011f39418911cb95caa49e
MatevzMiscic <matevz.miscic@gmail.com>
2020-12-03 10:07:31 +0100

    Napisal rešitev za drugo nalogo

--- day_3/day_3.ml
+++ day_3/day_3.ml
@@ -1,23 +1,40 @@
 
 
-let count map = 
+let count dx dy map = 
     let rec aux acc rest row = 
         match rest with
         | [] -> acc
         | l::ls -> 
-            if l.[(3 * row) mod (String.length l)] = '.' then aux acc ls (row + 1)
-            else aux (acc + 1) ls (row + 1)
+            if row mod dy = 0 then
+                let moves_right = row / dy in
+                if l.[(dx * moves_right) mod (String.length l)] = '.' then aux acc ls (row + 1)
+                else aux (acc + 1) ls (row + 1)
+            else aux acc ls (row + 1)
     in
     aux 0 map 0
 
 let naloga1 vsebina_datoteke =
     vsebina_datoteke
     |> String.split_on_char '\n'
-    |> count
+    |> count 3 1
     |> string_of_int
 
+let rec mul list = 
+    match list with
+    | [] -> 1
+    | x::xs -> x * (mul xs)
+
+let solve map = 
+    let slopes = [(1, 1); (3, 1); (5, 1); (7, 1); (1, 2)] in
+    let f (dx, dy) = count dx dy map
+    in
+    slopes |> List.map f |> mul
+
 let naloga2 vsebina_datoteke =
-    "1"
+    vsebina_datoteke
+    |> String.split_on_char '\n'
+    |> solve 
+    |> string_of_int
 
 let _ =
     let preberi_datoteko ime_datoteke =
