365bc984f3209b42e3414da02caf00614a1bb1b3
MatevzMiscic <matevz.miscic@gmail.com>
2020-12-04 10:51:32 +0100

    Rešil drugo nalogo

--- day_4/day_4.ml
+++ day_4/day_4.ml
@@ -16,12 +16,12 @@ let is_valid passport =
     if valid then 1
     else 0
 
-let count_valid list = 
+let count_valid f list = 
     let rec aux sum acc rest = 
         match rest with
         | [] -> sum + is_valid (String.concat " " acc)
         | x::xs ->
-            if x = "" then aux (sum + is_valid (String.concat " " acc)) [] xs
+            if x = "" then aux (sum + f (String.concat " " acc)) [] xs
             else aux sum (x::acc) xs
     in
     aux 0 [] list
@@ -29,10 +29,10 @@ let count_valid list =
 let naloga1 vsebina_datoteke =
     vsebina_datoteke
     |> String.split_on_char '\n'
-    |> count_valid
+    |> count_valid is_valid
     |> string_of_int
 
-let is_valid field str = 
+let is_field_valid field str = 
     if field = "byr" then
         let num = int_of_string str in
         String.length str = 4 && 1920 <= num && num <= 2002
@@ -42,26 +42,52 @@ let is_valid field str =
     else if field = "eyr" then
         let num = int_of_string str in
         String.length str = 4 && 2020 <= num && num <= 2030
-    else if field = "hgt" then (*TODO*)
-        let num = int_of_string str in
-        String.length str = 4 && 2020 <= num && num <= 2030
+    else if field = "hgt" then
+        let n = String.length str in
+        if n < 3 then false
+        else 
+            let num = int_of_string (String.sub str 0 (n-2)) in
+            if str.[n-2] = 'c' && str.[n-1] = 'm' then
+                150 <= num && num <= 193
+            else if str.[n-2] = 'i' && str.[n-1] = 'n' then
+                59 <= num && num <= 76
+            else false
     else if field = "hcl" then
         let rec hcl_ok str i = 
             if i = 0 then str.[0] = '#'
             else contains str.[i] ['0';'1';'2';'3';'4';'5';'6';'7';'8';'9';'a';'b';'c';'d';'e';'f'] && hcl_ok str (i-1)
-        in hcl_ok str ((String.length str) - 1)
+        in (String.length str = 7) && hcl_ok str 6
     else if field = "ecl" then
-        contains str ["amb"; "blu"; "brn"; "gry"; "grn"; "hzl;" "oth"]
+        contains str ["amb"; "blu"; "brn"; "gry"; "grn"; "hzl"; "oth"]
     else if field = "pid" then
-        let rec hcl_ok str i = 
-            if i = 0 then str.[0] = '#'
-            else contains str.[i] ['0';'1';'2';'3';'4';'5';'6';'7';'8';'9';'a';'b';'c';'d';'e';'f'] && hcl_ok str (i-1)
-        in hcl_ok str ((String.length str) - 1)
+        let rec pid_ok str i = 
+            if i < 0 then true
+            else contains str.[i] ['0';'1';'2';'3';'4';'5';'6';'7';'8';'9'] && pid_ok str (i-1)
+        in String.length str = 9 && pid_ok str 8
+    else true
 
-    
+let rec all_valid list = 
+    match list with
+    | [] -> true
+    | x::xs ->
+        if is_field_valid (List.hd x) (List.hd (List.tl x)) then all_valid xs
+        else
+            let a = print_endline ((List.hd x) ^ " " ^ (List.hd (List.tl x))); in
+            false
+
+let is_valid2 passport = 
+    let sep = List.map (String.split_on_char ':') (String.split_on_char ' ' passport) in
+    let fields = List.map List.hd sep in
+    let required = ["byr"; "iyr"; "eyr"; "hgt"; "hcl"; "ecl"; "pid"] in
+    let valid = contains_all required fields && all_valid sep in
+    if valid then 1
+    else 0
 
 let naloga2 vsebina_datoteke =
-    "skor"
+    vsebina_datoteke
+    |> String.split_on_char '\n'
+    |> count_valid is_valid2
+    |> string_of_int
 
 let _ =
     let preberi_datoteko ime_datoteke =
